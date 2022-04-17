47a7b4305cc80a7914ae6262191c55b156ac7a40
d4talouh <holdentlol@gmail.com>
2022-04-17 17:35:54 +0300

    Add django-channels to asgi

--- api/api/asgi.py
+++ api/api/asgi.py
@@ -1,14 +1,30 @@
 """
 ASGI config for api project.
+
 It exposes the ASGI callable as a module-level variable named ``application``.
+
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.conf.urls import url
 from django.core.asgi import get_asgi_application
+
+from gur.consumers import CourierConsumer, UserConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AllowedHostsOriginValidator(
+        URLRouter([
+            url(r"^socket/courier$", CourierConsumer.as_asgi()),
+            url(r"^socket/user$", UserConsumer.as_asgi()),
+        ])
+    ),
+})
